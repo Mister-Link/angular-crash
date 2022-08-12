403778bace674bfc17990ceb6824ff9433a965e9
Mister-Link <link.ray@usa.com>
2022-08-12 19:36:56 -0400

    2 files modified, 1 added, 1 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,8 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
+  declarations: [HeaderComponent],
   imports: [BrowserModule, FormsModule],
   bootstrap: [AppComponent],
 })
